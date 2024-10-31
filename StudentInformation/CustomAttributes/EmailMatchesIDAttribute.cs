af20d230e71d1c519a8b9f5f99a5d355ad0492cc
ABM Prottoy <abmlabs@outlook.com>
2024-11-01 02:33:21 +0600

    Add Email Match ID Attribute

---
+++ StudentInformation/CustomAttributes/EmailMatchesIDAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace StudentInformation.CustomAttributes
+{
+    public class EmailMatchesIDAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            // Check if email is provided
+            if (value == null || !(value is string email))
+            {
+                return new ValidationResult("Email is required.");
+            }
+
+            // Get the ID property value from the validation context
+            var idProperty = validationContext.ObjectType.GetProperty("ID");
+            if (idProperty == null)
+            {
+                return new ValidationResult("ID property not found.");
+            }
+
+            var idValue = idProperty.GetValue(validationContext.ObjectInstance) as string;
+            if (string.IsNullOrEmpty(idValue))
+            {
+                return new ValidationResult("ID is required.");
+            }
+
+            // Check if email matches the required format <ID>@student.aiub.edu
+            var expectedEmail = $"{idValue}@student.aiub.edu";
+            if (!email.Equals(expectedEmail, StringComparison.OrdinalIgnoreCase))
+            {
+                return new ValidationResult($"Email must match the format {idValue}@student.aiub.edu.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- StudentInformation/Models/Student.cs
+++ StudentInformation/Models/Student.cs
@@ -11,16 +11,17 @@ namespace StudentInformation.Models
     public class Student
     {
         [Required(ErrorMessage = "Please provide name")]
-        [RegularExpression(@"^[a-zA-Z .-]+$", ErrorMessage = "Name should only contains Small letters (a-z), Capital letters (A-Z) and Dot (.) or Hyphen (-)")]
+        [RegularExpression(@"^[a-zA-Z .-]+$", ErrorMessage = "Name may only contain Small letters (a-z), Capital letters (A-Z), Dot (.), Hyphen (-)")]
         [StringLength(10)]
         public string Name { get; set; }
 
         [Required]
-        [RegularExpression(@"^\d{2}-\d{5}-[1-3]$", ErrorMessage ="ID must be in AIUB ID Format. (XX-XXXXX-X)")]
+        [RegularExpression(@"^\d{2}-\d{5}-[1-3]$", ErrorMessage ="ID must be in AIUB ID format. (XX-XXXXX-X)")]
         public string ID { get; set; }
 
         [Required]
         [EmailAddress]
+        [EmailMatchesID(ErrorMessage = "Email must be in the AIUB student mail format (<ID>@student.aiub.edu) and match the given ID.")]
         public string Email { get; set; }
 
         [Required]
