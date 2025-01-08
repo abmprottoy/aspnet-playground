f0c666ff6d2147bec3835522f76849b19acd59a8
ABM Prottoy <abmlabs@outlook.com>
2025-01-08 23:54:29 +0600

    Add EF CF

---
+++ EntityFrameworkCodeFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntityFrameworkCodeFirst.WebApiApplication" Language="C#" %>
---
+++ EntityFrameworkCodeFirst/Migrations/202501080436582_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿namespace EntityFrameworkCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Stock = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
