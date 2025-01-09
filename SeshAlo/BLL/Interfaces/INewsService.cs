437b8eff4c4aaba3f2b7db3674d76c26ae81e052
ABM Prottoy <abmlabs@outlook.com>
2025-01-09 23:20:19 +0600

    Add SeshAlo News Portal

---
+++ SeshAlo/BLL/Interfaces/INewsService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.DTOs;
+
+namespace BLL.Interfaces
+{
+    public interface INewsService
+    {
+        void Add(NewsDTO newsDto);
+        void Update(NewsDTO newsDto);
+        NewsDTO Get(int id);
+        List<NewsDTO> GetAll();
+        void Delete(int id);
+        List<NewsDTO> SearchByTitle(string title);
+        List<NewsDTO> SearchByCategory(string category);
+        List<NewsDTO> SearchByDate(DateTime date);
+        List<NewsDTO> SearchByDateAndCategory(DateTime date, string category);
+        List<NewsDTO> SearchByDateAndTitle(DateTime date, string title);
+    }
+}
---
+++ SeshAlo/DAL/Repos/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.EF;
+
+namespace DAL.Repos
+{
+    public class Repository
+    {
+        protected NewsContext _context;
+        public Repository()
+        {
+            _context = new NewsContext();
+        }
+    }
+}
